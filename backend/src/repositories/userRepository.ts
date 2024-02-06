7821e4afa93a835a392acf339c8b42f73427f98a
shreya-codes <shreya.shrestha1999@gmail.com>
2024-02-06 10:46:12 +0545

    handle errors

--- backend/src/repositories/userRepository.ts
+++ backend/src/repositories/userRepository.ts
@@ -1,29 +1,29 @@
 import { IUser, UserModel } from "../models/User.model";
+import { APIError } from "../error/error";
+import { ErrorMap } from "../constants/error.constants";
 
 const insertUser = async (userData: IUser) => {
-  try {
-    const user = new UserModel({ ...userData });
-    const newUser = await user.save();
-    return newUser;
-  } catch (error) {
-    throw new Error(`Could not insert user: ${error.message}`);
-  }
+  const user = new UserModel({ ...userData });
+  const newUser = await user.save();
+  return newUser;
 };
 
 const getUser = async (selector = {}) => {
-  try {
-    const alreadyExistsUser = await UserModel.findOne({
-      ...selector,
-      deleted: { $ne: true },
-    });
-
-    if (!alreadyExistsUser) {
-      return false;
-    }
-    return alreadyExistsUser;
-  } catch (error) {
-    throw new Error(`Could not get user: ${error.message}`);
+  const alreadyExistsUser = await UserModel.findOne({
+    ...selector,
+    deleted: { $ne: true },
+  });
+  if (!alreadyExistsUser) {
+    throw new APIError(
+      ErrorMap.NotFoundError("User"),
+      `Could not find user with email${{ ...selector }}} `
+    );
   }
+
+  if (!alreadyExistsUser) {
+    return false;
+  }
+  return alreadyExistsUser;
 };
 
 export { insertUser, getUser };
--- backend/src/services/auth.services.ts
+++ backend/src/services/auth.services.ts
@@ -3,50 +3,49 @@ import { IUser } from "../models/User.model";
 import { insertUser, getUser } from "../repositories/userRepository";
 import { signJwt } from "../services/jwt.services";
 import bcrypt from "bcrypt";
+import { APIError } from "../error/error";
+import { ErrorMap } from "../constants/error.constants";
 
 const registerUserService = async (userData: IUser) => {
-  try {
-    const existingUser = await getUser({ email: userData.email });
-    if (existingUser) {
-      throw new Error(`User already exists with email ${userData.email}`);
-    }
-    const user = await insertUser(userData);
-    const token = await signJwt(user._id.toString());
-    return { token, user };
-  } catch (error) {
-    throw new Error(`Could not register user: ${error.message}`);
+  const existingUser = await getUser({ email: userData.email });
+  if (existingUser) {
+    throw new APIError(
+      ErrorMap.NotFoundError("User"),
+      `User already exists with email${userData.email} `
+    );
   }
+  const user = await insertUser(userData);
+  const token = await signJwt(user._id.toString());
+  return { token, user };
 };
 
 const loginUserService = async (email: string, password: string) => {
-  try {
-    const user = await findByCredentials(email, password);
-    if (!user) {
-      throw new Error("User not found");
-    }
-    const token = await signJwt(user._id.toString());
-
-    return { user: { email }, token };
-  } catch (error) {
-    throw new Error(`Could not login user: ${error.message}`);
+  const user = await findByCredentials(email, password);
+  if (!user) {
+    throw new APIError(
+      ErrorMap.NotFoundError("User"),
+      `Could not find user with email${email}} `
+    );
   }
+  const token = await signJwt(user._id.toString());
+
+  return { user: { email }, token };
 };
 
 const findByCredentials = async (email: string, password: string) => {
-  try {
-    const user = await getUser({ email });
-    if (!user) {
-      throw new Error(`User with email ${email} does not exist`);
-    }
-
-    const isPasswordMatch = await bcrypt.compare(password, user.password);
-    if (!isPasswordMatch) {
-      throw new Error("Incorrect Password");
-    }
-    return user;
-  } catch (error) {
-    throw new Error(`Error finding user by credentials: ${error.message}`);
+  const user = await getUser({ email });
+  if (!user) {
+    throw new APIError(
+      ErrorMap.NotFoundError("User"),
+      `Could not find user with email${email}} `
+    );
   }
+
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+  if (!isPasswordMatch) {
+    throw new Error("Incorrect Password");
+  }
+  return user;
 };
 
 export { registerUserService, loginUserService, findByCredentials };
