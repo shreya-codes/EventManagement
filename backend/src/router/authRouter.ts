2f36bea69e2859c0bba605d192f6789d259c8e36
shreya-codes <shreya.shrestha1999@gmail.com>
2024-02-06 10:32:08 +0545

    fix auth

--- backend/src/router/authRouter.ts
+++ backend/src/router/authRouter.ts
@@ -1,9 +1,7 @@
 import { Router } from "express";
 import { registerUser, loginUser } from "../controller/auth.controller";
-import { authMiddleware } from "../middleware/authMiddleware";
 
 const authRouter = Router();
-authRouter.use(authMiddleware);
 authRouter.post("/signup", registerUser);
 authRouter.post("/login", loginUser);
 
