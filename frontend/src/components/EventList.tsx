8ff373c2aed9ec5dbdfb279c3ad401c285a1f752
shreya-codes <shreya.shrestha1999@gmail.com>
2024-02-06 11:20:44 +0545

    handle empty list

--- frontend/src/components/EventList.tsx
+++ frontend/src/components/EventList.tsx
@@ -68,30 +68,33 @@ const EventList = () => {
     <div className="min-h-full max-w-screen-xl mx-auto mt-4 space-y-6 text-gray-600 ">
       <div className="w-full bg-white shadow p-4 py-6 sm:p-6 sm:rounded-lg">
         {isLoading && <p>Loading...</p>}
-        {error && <p>Error: {error.message}</p>}
-        <table className="w-full table-auto text-sm text-left">
-          <thead className="bg-gray-50 text-gray-600 font-medium border-b">
-            <tr>
-              <th className="py-3 px-6">Event Name</th>
-              <th className="py-3 px-6">Description</th>
-              <th className="py-3 px-6">Location</th>
-              <th className="py-3 px-6">Start Date</th>
-              <th className="py-3 px-6">End Date</th>
-              <th className="py-3 px-6">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {events?.events.map((event: IEvent, index: number) => (
-              <EventRow
-                key={event._id}
-                event={event}
-                index={index}
-                onEditClick={handleEditClick}
-                onDeleteClick={handleDeleteClick}
-              />
-            ))}
-          </tbody>
-        </table>
+        {events && events.events && events.events.length === 0 ? (
+          <p>No events available</p>
+        ) : (
+          <table className="w-full table-auto text-sm text-left">
+            <thead className="bg-gray-50 text-gray-600 font-medium border-b">
+              <tr>
+                <th className="py-3 px-6">Event Name</th>
+                <th className="py-3 px-6">Description</th>
+                <th className="py-3 px-6">Location</th>
+                <th className="py-3 px-6">Start Date</th>
+                <th className="py-3 px-6">End Date</th>
+                <th className="py-3 px-6">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {events?.events.map((event: IEvent, index: number) => (
+                <EventRow
+                  key={event._id}
+                  event={event}
+                  index={index}
+                  onEditClick={handleEditClick}
+                  onDeleteClick={handleDeleteClick}
+                />
+              ))}
+            </tbody>
+          </table>
+        )}
 
         {"_id" in editEvent && editEvent._id && (
           <div className="fixed top-0 left-0 flex items-center justify-center bg-black bg-opacity-50">
